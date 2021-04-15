ce2d8aacfd83bf04bf32c1571de1ec36ac884ec3
leonelvis <leonelvisjgg@gmail.com>
2021-04-15 12:56:18 -0500

    commit-java-news

--- base/src/main/java/com/henry/news/base/repository/NoticiasRepository.java
+++
@@ -1,9 +0,0 @@
-package com.henry.news.base.repository;
-
-import com.henry.news.base.model.Noticias;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoticiasRepository extends JpaRepository<Noticias, Integer> {
-}
